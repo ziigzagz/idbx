d01d4e6d995f109a241f377f6aa23106f5e91885
ziigzagz <iffan.h@ideabranch.co.th>
2024-03-08 13:14:01 +0700

    Add title and data variables to Home controller

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -3,18 +3,23 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
+	private $data;
+	private $title;
     public function __construct()
     {
         parent::__construct();
         $this->load->model('Comp_part');
         $this->load->model('User');
+		$this->title = "Home";
+		$this->data = array(
+			'title' => $this->title
+		);
         date_default_timezone_set("Asia/Bangkok");
     }
     public function index()
     {
         $this->load->view('css');
         $this->load->view('js');
-        $this->load->view('Home');
-		
+        $this->load->view('Home/Index', $this->data);
     }
 }
